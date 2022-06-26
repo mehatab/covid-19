ef1a4eaafc2eb9e59d4e32707a2ac1f93dc04f4e
mahatab <mahatab.shaikh@cogoport.com>
2022-06-26 10:58:45 +0530

    Revert "Merge pull request #3 from GeorgCantor/master"
    This reverts commit 0ad5ae9970d841d69b4a8ef5fee040ed2b1ace53, reversing
    changes made to 00d722e8d88bab9892cc707a2f1dbb44a59ee23b.

--- data/src/main/java/info/covid/data/network/RetrofitClient.kt
+++ data/src/main/java/info/covid/data/network/RetrofitClient.kt
@@ -2,17 +2,20 @@ package info.covid.data.network
 
 import com.squareup.moshi.Moshi
 import info.covid.data.converters.CountryCodeConverters
+import info.covid.data.converters.MoshiCurrencyConverter
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object RetrofitClient {
-    const val BASE_URL = "https://api.covid19india.org"
-    const val GLOBAL_BASE_URL = "https://corona.lmao.ninja/v2/"
 
-    fun get(baseUrl: String = BASE_URL) = Retrofit.Builder()
+object RetrofitClient {
+   const val BASE_URL = "https://api.covid19india.org"
+   const val GLOBAL_BASE_URL = "https://corona.lmao.ninja/v2/"
+
+    fun get(baseUrl : String?= BASE_URL) = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
+
 
     private val moshi = Moshi.Builder()
         .add(CountryCodeConverters)
